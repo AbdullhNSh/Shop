63f2dbd40aaa38b4e91c61887e9492f76807650e
sahilgrg1 <gargsahil898@gmail.com>
2019-05-02 23:39:00 +0530

    Fragmented

--- News/app/src/main/java/com/example/news/MainActivity.kt
+++ News/app/src/main/java/com/example/news/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.news
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewpager.adapter = ViewPagerAdapter(supportFragmentManager)
+        tablayout.setupWithViewPager(viewpager)
+    }
+    class ViewPagerAdapter(fm : FragmentManager?): FragmentPagerAdapter(fm)
+    {
+        override fun getItem(p0: Int): Fragment {
+            return when(p0)
+            {
+                0 -> A()
+                1 -> B()
+                else ->
+                {
+                    A()
+                }
+            }
+        }
+
+        override fun getCount(): Int = 2
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position)
+            {
+                 0 -> "News \t \t"
+                1 -> "Notes"
+
+                else -> "Others"
+            }
+        }
     }
 }
