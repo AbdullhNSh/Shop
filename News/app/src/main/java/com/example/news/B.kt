27e8e9995e9d47dc804dbf08452e723d5e536fdd
sahilgrg1 <gargsahil898@gmail.com>
2019-05-03 13:32:05 +0530

    update

--- News/app/src/main/java/com/example/news/B.kt
+++ News/app/src/main/java/com/example/news/B.kt
@@ -1,5 +1,6 @@
 package com.example.news
 
+import android.app.AlertDialog
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -10,7 +11,18 @@ import android.view.ViewGroup
 
 class B : Fragment()
 {
+    private val notes = ArrayList<Notes?>()
+    private val notesadapter = NotesAdapter(notes)
+    private val dialogview by lazy {
+        layoutInflater.inflate(R.layout.layout_diagnol,null,false)
+    }
+    private val noteAlert by lazy {
+
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_b,container,false)
+
+
     }
+
 }
