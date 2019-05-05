3f2d64b977dd80a2f3cfdd370b8971fbdf8bd03a
sahilgrg1 <gargsahil898@gmail.com>
2019-05-05 10:34:58 +0530

    restart

--- News/app/src/main/java/com/example/news/MainActivity.kt
+++ News/app/src/main/java/com/example/news/MainActivity.kt
@@ -7,38 +7,14 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_b.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewpager.adapter = ViewPagerAdapter(supportFragmentManager)
-        tablayout.setupWithViewPager(viewpager)
-    }
-    class ViewPagerAdapter(fm : FragmentManager?): FragmentPagerAdapter(fm)
-    {
-        override fun getItem(p0: Int): Fragment {
-            return when(p0)
-            {
-                0 -> A()
-                1 -> B()
-                else ->
-                {
-                    A()
-                }
-            }
-        }
 
-        override fun getCount(): Int = 2
-        override fun getPageTitle(position: Int): CharSequence? {
-            return when(position)
-            {
-                 0 -> "News"
-                1 -> "Notes"
-
-                else -> "Others"
-            }
-        }
     }
+
 }
--- News/app/src/main/java/com/example/news/Notes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.news
-
-class Notes(
-    var title : String = "",
-    var des : String = ""
-   )
